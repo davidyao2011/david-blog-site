ed0ccaa9a701bed4d0b5e17b5473fafab18b3bc2
David Yao <davidyao2011@hotmail.com>
2020-06-19 12:30:10 +1200

    add action

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
     <Layout>
       <Metadata title="contact" description="This is my contact page" />
       <h1>Contact Us</h1>
-      <ContactForm title="https://www.flexyform.com/f/44b4cde2bdb01787fd304d99dc7c115099b7ed21" />
+      <ContactForm action="https://www.flexyform.com/f/44b4cde2bdb01787fd304d99dc7c115099b7ed21" />
     </Layout>
   )
 }
